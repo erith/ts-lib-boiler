2d28eef30a692cf0f9081c5a279fbc7db3162324
erith <erith9@gmail.com>
2021-10-20 20:49:28 +0900

    init commit

---
+++ build/Button/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+function Button(_a) {
+    var children = _a.children;
+    return React.createElement("button", { type: "button" }, children);
+}
+export default Button;
+//# sourceMappingURL=Button.js.map
---
+++ build/index.es.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Button(_a) {
+    var children = _a.children;
+    return React.createElement("button", { type: "button" }, children);
+}
+
+export { Button };
+//# sourceMappingURL=index.es.js.map
